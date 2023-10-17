6928201424cb811739d5c6d29bccbca75aa39b4f
FireboltLegend <aik210000@utdallas.edu>
2023-10-16 21:04:06 -0500

    GameMenu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayDinoGame()
+    {
+        SceneManager.LoadScene("DinoGame");
+    }
+
+    public void PlayFlappy()
+    {
+        SceneManager.LoadScene("FlappyBird");
+    }
+    public void PlayDK()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+    public void PlayAsteroid()
+    {
+        SceneManager.LoadScene("Asteroids");
+    }
+}
